569d25d4f11cdc01080b1fd677d35ffb3e9aeb71
atrax <atrax@prtn.email>
2020-07-25 12:34:25 +0200

    Fix version number in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 if __name__ == "__main__":
     setup(
         name="cmus-syncthing",
-        version="0.1",
+        version="0.6.1",
         packages=find_packages(),
         install_requires=open("requirements.txt").read().splitlines(),
         entry_points={
